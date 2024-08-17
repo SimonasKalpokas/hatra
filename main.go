f58f40ee0592a171f1440daa87cc92d948b3bb57
SimonasKalpokas1 <simonas.kalpokas@gmail.com>
2024-08-17 19:34:35 +0300

    WIP

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // time
@@ -14,7 +20,48 @@ type Date struct {
 	Day   int
 }
 
+// Parses Date that is in format of YYYY-MM-DD
+//
+// TODO: move to separate module?
+func ParseDate(input string) (Date, error) {
+	parts := strings.Split(input, "-")
+
+	if len(parts) != 3 || 
+   	   len(parts[0]) != 4 || 
+	   len(parts[1]) != 2 || 
+	   len(parts[2]) != 2 {
+		return Date{}, errors.New(fmt.Sprintf("bad string format: %s", input))
+	}
+	
+	year, err := strconv.Atoi(parts[0])
+	if err != nil || year < 0 {
+		return Date{}, errors.New(fmt.Sprintf("bad string format: %s", input))
+	}
+
+	month, err := strconv.Atoi(parts[1])
+	if err != nil || month > 12 || month < 1 {
+		return Date{}, errors.New(fmt.Sprintf("bad string format: %s", input))
+	}
+
+	day, err := strconv.Atoi(parts[2])
+	if err != nil || month > 12 || month < 1 {
+		return Date{}, errors.New(fmt.Sprintf("bad string format: %s", input))
+	}
+
+	return Date {
+		Year: year,
+		Month: month,
+		Day: day,
+	}, nil
+
+}
+
 // data
+
+type Habit struct {
+	Name	string
+	Days	[]Date
+}
 
 type DayNugget struct {
 	Date	Date
@@ -30,6 +77,21 @@ type HabitPageData struct {
 // display
 
 func main() {
+	dataFilePaths, _ := filepath.Glob("./data/*.txt")
+
+	for _, filePath := range dataFilePaths {
+		exp, _ := regexp.Compile("data\\/(\\w+)\\.txt")
+		name := exp.FindStringSubmatch(filePath)[1]
+		_ = name
+
+		content, _ := os.ReadFile(filePath)
+
+		lines := strings.Split(string(content), "\n")
+		fmt.Println(lines)
+	}
+}
+
+func main1() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
